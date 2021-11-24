96624b034a6671c98532a549cab4373ae1e40d8a
seralma2 <88503220+seralma2@users.noreply.github.com>
2021-11-24 20:03:00 +0100

    Update NRF_EMERGENCY.ino

--- NRF_EMERGENCY.ino
+++ NRF_EMERGENCY.ino
@@ -16,7 +16,6 @@ int longitud = 0;
 const uint64_t pipe = 0xF0F0F0F0E3LL;
 char m_send[128] = "";
 char data[16]="" ;
-//seleccionador de pipe
 int contador;
 void setup(void)
 {
@@ -36,54 +35,40 @@ void setup(void)
 void loop(void)
 {
   contador = 1;
-  //Serial.println(value_boton);
   int value_boton = digitalRead(boton);
   int value_boton_fin = digitalRead(boton_fin);
-  //Serial.println(value_boton_fin);
   if(value_boton == HIGH && contador == 1 ){
     msg_ini();
   }
   if(value_boton_fin == HIGH ){
     msg_fin();
   }
-  //solo acepta []
   
   if(longitud > 0){
       radio.openWritingPipe(pipe);
       Serial.println(m_send);
       if(radio.write(m_send, sizeof (m_send))){
-        Serial.println("el m_send es: ");
         Serial.print(m_send);
           while (radio.isAckPayloadAvailable()) {
-            Serial.println("aqui entro??");
             int32_t msgSize = radio.getDynamicPayloadSize();  //ack payload length is dynamic
             byte msgIn[msgSize];
-            Serial.print("ACK: ");
             
             Serial.println(radio.read(&msgIn, msgSize));
           }
       }
-      Serial.println("FIN");
       radio.startListening();
       longitud = 0;
     }
 }
 void msg_ini(){
     radio.stopListening();
-  //radio.write(data, 16);
-  //Serial.println("he entrado");
-  doc["Emergency"]= "2";
-  //doc["2"]= "Hello";
+  doc[/*KEY*/]= /*Value*/;
   serializeJson(doc, msg);
   Serial.println(msg);
   doc.clear();
   longitud = msg.length()+1;
-  //Serial.print ("La longitud es :");
-  //Serial.println(longitud);
-  //convertidos el string en cadena para poder enviar
   msg.toCharArray(m_send, longitud);
   msg = "";
-    //Serial.println("Se ha serializado");
     contador = 0;
   }
 
@@ -91,16 +76,11 @@ void msg_fin(){
     radio.stopListening();
   //radio.write(data, 16);
   Serial.println("he entrado");
-  doc["Emergency"]= "FIN";
-  //doc["2"]= "Hello";
+  doc[/*KEY*/]= /*Value*/;
   serializeJson(doc, msg);
   Serial.println(msg);
   doc.clear();
   longitud = msg.length()+1;
-  Serial.print ("La longitud es :");
-  Serial.println(longitud);
-  //convertidos el string en cadena para poder enviar
   msg.toCharArray(m_send, longitud);
   msg = "";
-    Serial.println("Se ha serializado");
   }
